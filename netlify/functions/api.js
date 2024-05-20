34dc3f07dbbbfd8c25fc1089c4927aa569f8727c
Dan_Kerslager <dan.kerslager@tul.cz>
2024-05-20 19:40:45 +0200

    Autho fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,9 +3,9 @@
 //example call: curl -X POST https://pocasitest.netlify.app/.netlify/functions/api -H "Content-Type: application/json" -d "{\"email\":\"dan.kerslager@tul.cz\",\"password\":\"TestTest1\",\"location\":\"New York\"}"
 
 
-const { AuthenticationClient } = require('auth0');
+const auth0 = require('auth0.js');
 
-const auth0 = new AuthenticationClient({
+const auth0Client = new auth0.AuthenticationClient({
   domain: 'dev-cz1xfrqlz4gbz633.us.auth0.com',
   clientId: 'HJJSClNdpO05vRw0oYXbSi9eCvkKMUFd',
   clientSecret: 'Ro_meg_dZnC2No76c61tHeGA46CdSaThHNZ-5AiHdw22GPbTpLFe_kAsaFmC1ohQ'
@@ -27,10 +27,10 @@ exports.handler = async function(event, context) {
       }
   
       // Authenticate user against Auth0
-      const authResult = await auth0.passwordGrant({
+      const authResult = await auth0Client.oauth.passwordGrant({
         username: email,
         password: password,
-        audience: 'https://dev-cz1xfrqlz4gbz633.us.auth0.com/api/v2/',
+        //audience: 'https://dev-cz1xfrqlz4gbz633.us.auth0.com/api/v2/',
         scope: 'openid'
       });
   
