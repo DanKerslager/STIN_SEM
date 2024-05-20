92fd03ed6d3eb49a0a7a46b2e9a107b3734b6c52
Dan_Kerslager <dan.kerslager@tul.cz>
2024-05-20 19:05:40 +0200

    auth0 fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,14 @@
 // netlify/functions/api.js
 
-//example call: curl -X POST https://pocasitest.netlify.app/.netlify/functions/api -H "Content-Type: application/json" -d "{\"email\":\"user@example.com\",\"password\":\"password123\",\"location\":\"New York\"}"
+//example call: curl -X POST https://pocasitest.netlify.app/.netlify/functions/api -H "Content-Type: application/json" -d "{\"email\":\"dan.kerslager@tul.cz\",\"password\":\"TestTest1\",\"location\":\"New York\"}"
 
 
 const { AuthenticationClient } = require('auth0');
 
 const auth0 = new AuthenticationClient({
   domain: 'dev-cz1xfrqlz4gbz633.us.auth0.com',
-  clientId: 'GcpBuKna5egJWpvRWTEfbKFte1mywkA8',
-  clientSecret: 'fH6s_Tk7kcbDNzH1BU2wBzlbd1--CCmIaVfv4LZNSjljZrSDdVx7_dXdGqIqnzFM'
+  clientId: 'HJJSClNdpO05vRw0oYXbSi9eCvkKMUFd',
+  clientSecret: 'Ro_meg_dZnC2No76c61tHeGA46CdSaThHNZ-5AiHdw22GPbTpLFe_kAsaFmC1ohQ'
 });
 
 exports.handler = async function(event, context) {
@@ -30,7 +30,7 @@ exports.handler = async function(event, context) {
       const authResult = await auth0.passwordGrant({
         username: email,
         password: password,
-        audience: 'YOUR_AUTH0_API_AUDIENCE',
+        audience: 'https://dev-cz1xfrqlz4gbz633.us.auth0.com/api/v2/',
         scope: 'openid'
       });
   
