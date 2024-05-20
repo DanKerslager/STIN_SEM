01325b31c44a658ed32a0c7401c8c21952780cc8
Dan_Kerslager <dan.kerslager@tul.cz>
2024-05-20 19:56:26 +0200

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,14 +2,12 @@
 
 //example call: curl -X POST https://pocasitest.netlify.app/.netlify/functions/api -H "Content-Type: application/json" -d "{\"email\":\"dan.kerslager@tul.cz\",\"password\":\"TestTest1\",\"location\":\"New York\"}"
 
+const { AuthenticationClient } = require('auth0');
 
-const auth0 = require('auth0-js');
-
-const webAuth = new auth0.WebAuth({
+const auth0 = new AuthenticationClient({
     domain: 'dev-cz1xfrqlz4gbz633.us.auth0.com',
-    clientID: 'HJJSClNdpO05vRw0oYXbSi9eCvkKMUFd',
-    responseType: 'token',
-    redirectUri: "https://pocasitest.netlify.app/callback", // Replace with your actual callback URL
+    clientId: 'HJJSClNdpO05vRw0oYXbSi9eCvkKMUFd',
+    clientSecret: 'Ro_meg_dZnC2No76c61tHeGA46CdSaThHNZ-5AiHdw22GPbTpLFe_kAsaFmC1ohQ'
 });
 
 exports.handler = async function(event, context) {
@@ -27,26 +25,19 @@ exports.handler = async function(event, context) {
             };
         }
 
-        // Authenticate user using Auth0
-        webAuth.login({
-            realm: 'Username-Password-Authentication', // This is the default database connection name in Auth0
+        // Authenticate user against Auth0
+        const authResult = await auth0.passwordGrant({
             username: email,
-            password: password
-        }, (err, authResult) => {
-            if (err) {
-                // Authentication failed, handle error
-                return {
-                    statusCode: 400,
-                    body: JSON.stringify({ message: `Error: ${err.description}` }),
-                };
-            } else {
-                // Authentication successful, handle result
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ message: 'Authentication successful', user: authResult }),
-                };
-            }
+            password: password,
+            audience: 'https://dev-cz1xfrqlz4gbz633.us.auth0.com/api/v2/',
+            scope: 'openid'
         });
+
+        // Example response
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Authentication successful', user: authResult.user }),
+        };
     } catch (error) {
         return {
             statusCode: 400,
